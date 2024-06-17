97b192a4f2c178689c7e2930fcd4f60522e5c914
HYV <zinks1998@gmail.com>
2024-06-17 17:54:33 +0900

    Add : MenuUI
    게임 메뉴 UI의 틀을 만듦

--- Assets/Scripts/UIs/Btns/LoadSceneBtns.cs
+++ Assets/Scripts/UIs/Btns/LoadSceneBtns.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Btns : MonoBehaviour
+public class LoadSceneBtns : MonoBehaviour
 {
     private string start = "StartScene";
     private string main = "MainScene";
-    public void StartBtn()
+
+    public void OnStartBtn()
     {
         SceneManager.LoadScene(main);
+    }
+
+    public void OnReStartBtn()
+    {
+        SceneManager.LoadScene(start);
     }
 }
---
+++ Assets/Scripts/UIs/Btns/MenuBtn.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class MenuBtn : MonoBehaviour
+{
+    [SerializeField] private GameObject canvas;
+    [SerializeField] private GameObject menuUI;
+
+    private GameObject menu = null;
+
+    public void OnMenuBtn()
+    {
+        if (menu == null)
+        {
+            menu = Instantiate(menuUI, canvas.transform);
+        }
+        else if (menu != null)
+        {
+            Destroy(menu);
+        }
+    }
+}
